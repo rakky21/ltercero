ba687b1c04afeb329e47b10bbc14724472ecb16c
rakky21 <rakky26@gmail.com>
2021-10-08 12:18:33 -0400

    added some info

---
+++ assets/js/game2.js
@@ -0,0 +1,28 @@
+// window.alert("This is an alert!@");
+// window.prompt("What is your robot's name?");
+// this creates a function named "fight"
+
+
+
+
+var playerName = window.prompt("Whatr is your robt's name?");
+console.log(playerName);
+window.alert(playerName);
+
+
+function fight() {
+    window.alert("the fight has begun!");
+    // fight();
+};
+
+
+var playerName = "Tony the Robot";
+
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
